0587962357d5026b8f1cba936250a47e167d6646
ijishnupr <ijishnupr@gmail.com>
2024-03-22 07:25:58 +0530

    req

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from rest_framework.authtoken.admin import TokenAdmin as BaseTokenAdmin
+from rest_framework.authtoken.models import Token
 from .models import *
 # Register your models here.
 admin.site.register(User)
@@ -6,3 +8,9 @@ admin.site.register(CustomerDetails)
 admin.site.register(SalesTeamDetails)
 admin.site.register(ConsultantInfo)
 admin.site.register(DoctorDetails)
+
+class CustomTokenAdmin(BaseTokenAdmin):
+    search_fields = ('key', 'user__username')  # Adjust as needed
+
+# Register TokenAdmin with your custom subclass
+admin.site.register(Token, CustomTokenAdmin)
